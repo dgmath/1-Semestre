f5b10831f150a4ef2df40a4c57e8f2246e1febc8
Matheus Dias Gomes <53025781823@info.local>
2023-04-03 17:22:29 -0300

    atividade 03-04

---
+++ Calculadora-programa/Program.cs
@@ -0,0 +1,164 @@
+﻿// //calculadora
+
+// //Algoritmo
+
+// //1. informar a operacao
+// //2. informar o primeiro numero
+// //3. infrmar o segundo numero
+// //4. processar os dados
+// //5. exibir resultado
+
+// //entrada
+
+// Console.WriteLine(@$"
+// ---------------------------
+// | Programa de calculadora |
+// |                         |
+// |   Informe a operação    |
+// |                         |
+// |   (+) Soma              |
+// |   (-) Subtração         |
+// |   (*) Multiplicação     |
+// |   (/) Divisão           |
+// |                         |
+// ---------------------------
+// ");
+
+// char operacao = char.Parse(Console.ReadLine());
+
+// Console.WriteLine($"Informe o primeiro número: ");
+// double num1 = double.Parse(Console.ReadLine());
+
+// Console.WriteLine($"Informe o segundo número: ");
+// double num2 = double.Parse(Console.ReadLine());
+
+// double resultado = 0;
+
+// //processamento
+
+// switch (operacao)
+// {
+//     case '+':
+//     resultado = (num1 + num2);
+//     Console.WriteLine($"O resultado da Conta é {resultado}");
+    
+//         break;
+
+//     case '-':
+//         resultado = (num1 - num2);
+//             Console.WriteLine($"O resultado da Conta é {resultado}");
+//         break;
+
+//     case '*':
+//         resultado = (num1 * num2);
+//             Console.WriteLine($"O resultado da Conta é {resultado}");
+//         break;
+
+//     case '/':
+//         resultado = (num1 / num2);
+//             Console.WriteLine($"O resultado da Conta é {resultado}");
+//         break;
+
+//     default:
+//             Console.WriteLine($"Operação invalida, repita o processo corretamente.");
+//         break;
+// }
+
+//Coca cola
+//Pepsi
+//Fanta
+//Monster
+
+//Crie um menu de opções e peça para o usuário escolher uma das opções
+//Dependendo da opção, exiba uma mensagem se gostaria de acrescentar gelo
+//Se sim, exiba uma mensagem que a bebida em questao sera com adicional de gelo 
+//Caso o contrario a bebida é sem gelo adicional.
+
+Console.WriteLine(@$"
+-----------------------
+|  Seja bem vindo ao  |
+|   Lar das bebidas   |                    
+-----------------------
+");
+
+Console.WriteLine(@$"
+----------------------
+|      Cardápio:     |
+|                    |
+|----> Coca cola (1) |
+|----> Pepsi (2)     |
+|----> Fanta (3)     |
+|----> Monster (4)   |
+|                    |
+|  Informe a bebida  |
+|   de sua escolha   |
+|     em número:     | 
+---------------------|
+");
+int bebida = int.Parse(Console.ReadLine()!);
+
+switch (bebida)
+{
+    case 1:
+    Console.WriteLine($"Deseja adicionar gelo? Digite 1 para sim e 2 pra não:");
+    int gelo = int.Parse(Console.ReadLine()!);
+   
+    if (gelo == 1)
+    {
+    Console.WriteLine($"Coca cola com gelo.");}
+
+    else
+    {
+        Console.WriteLine($"Coca cola sem gelo");
+    }
+        break;
+    
+        case 2:
+    Console.WriteLine($"Deseja adicionar gelo? Digite 1 para sim e 2 pra não:");
+    int gelo2 = int.Parse(Console.ReadLine()!);
+   
+    if (gelo2 == 1)
+    {
+    Console.WriteLine($"Pepsi com gelo.");}
+
+    else
+    {
+        Console.WriteLine($"Pepsi sem gelo");
+    }
+        break;
+
+            case 3:
+    Console.WriteLine($"Deseja adicionar gelo? Digite 1 para sim e 2 pra não:");
+    int gelo3 = int.Parse(Console.ReadLine()!);
+   
+    if (gelo3 == 1)
+    {
+    Console.WriteLine($"Fanta com gelo.");}
+
+    else
+    {
+        Console.WriteLine($"Fanta sem gelo");
+    }
+        break;
+
+            case 4:
+    Console.WriteLine($"Deseja adicionar gelo? Digite 1 para sim e 2 pra não:");
+    int gelo4 = int.Parse(Console.ReadLine()!);
+   
+    if (gelo4 == 1)
+    {
+    Console.WriteLine($"Monster com gelo.");}
+
+    else
+    {
+        Console.WriteLine($"Monster sem gelo");
+    }
+        break;
+
+    default:
+    Console.WriteLine($"Desculpe, é preciso que você informe o valor novamente.");
+    
+        break;
+}
+
+
---
+++ Estrutura Switch-Case/Program.cs
@@ -0,0 +1,64 @@
+﻿//switch (//valor que quero comparar)
+
+//    case x: 
+    //bloco de codigo a ser executado
+ //   break;
+
+//    case y: 
+    //bloco de codigo a ser executado
+ //   break;
+    
+ //   case z: 
+    //bloco de codigo a ser executado
+ //   break;
+
+ //   default: //encerramento
+    //bloco de codigo ou mensagem 
+  //  break;
+
+//programa para identificar qual o dia da semana
+
+Console.WriteLine($"Informe o numero correspondente ao dia da semana: ");
+int diaSemana = int.Parse(Console.ReadLine());
+
+switch (diaSemana)
+{
+    case 1:
+    //bloco de codigos
+    Console.WriteLine($"Domingo");
+    break;
+
+    case 2:
+    //bloco de codigos
+    Console.WriteLine($"Segunda-Feira");
+    break;
+
+    case 3:
+    //bloco de codigos
+    Console.WriteLine($"Terça-Feira");
+    break;
+
+    case 4:
+    //bloco de codigos
+    Console.WriteLine($"Quarta-Feira");
+    break;
+
+    case 5:
+    //bloco de codigos
+    Console.WriteLine($"Quinta-Feira");
+    break;
+
+    case 6:
+    //bloco de codigos
+    Console.WriteLine($"Sexta-Feira");
+    break;
+
+    case 7:
+    //bloco de codigos
+    Console.WriteLine($"Sabado");
+    break;
+
+    default:
+    Console.WriteLine($"Desculpe, o número informado não é válido, informe um número de 1 a 7 e tente novamente.");
+    break;
+}
